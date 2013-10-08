073aa58ff418e82217eb22bf59ee2385fe4e47d0
ecalifornica <queenin+git@gmail.com>
2013-10-08 01:31:15 -0700

    remove .pyc files

--- tester.py
+++ tester.py
@@ -1,8 +1,14 @@
 import recipe
 import brew
 
-r = recipe.Recipe()
-b = brew.Brew(r)
-b.loop()
-print('test finished\n')
+import unittest
 
+class my_test(unittest.TestCase):
+    def test_function(self):
+        self.recipe = recipe.Recipe()
+        brewing_session = brew.Brew(recipe)
+        brewing_session.loop()
+        print('test finished\n')
+
+if __name__ == '__main__':
+    unittest.main()
