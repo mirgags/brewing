d89c9b2a838d58ab4fbf383ef2b6c66f2a1021c6
ecalifornica <queenin+git@gmail.com>
2013-10-08 15:40:58 -0700

    tester.py update

--- tester.py
+++ tester.py
@@ -5,6 +5,25 @@ import grain
 import yeast
 import pickle
 
+class my_test(unittest.TestCase):
+    def test_function(self):
+        #
+        self.recipe = recipe.Recipe()
+        # I modified select_ingredient to take programmatic input
+        #self.recipe.select_ingredient('H')
+        # But now I am going to try just appending to the lists
+        # I forgot that the hops list is objects, not strings
+        # hmm...zzz
+        #self.recipe.hops = ['Select', 'Galaxy']
+        brewing_session = brew.Brew(recipe)
+        #brewing_session.loop()
+        print('test finished\n')
+
+if __name__ == '__main__':
+    unittest.main()
+
+#mark, you said this stuff was trash now?
+'''
 i = []
 
 <<<<<<< HEAD
@@ -30,20 +49,4 @@ b.start_timer()
 b.loop()
 print('test finished\n')
 >>>>>>> 60f97891c14c2560277173f44be2273da8367b6d
-
-class my_test(unittest.TestCase):
-    def test_function(self):
-        #
-        self.recipe = recipe.Recipe()
-        # I modified select_ingredient to take programmatic input
-        #self.recipe.select_ingredient('H')
-        # But now I am going to try just appending to the lists
-        # I forgot that the hops list is objects, not strings
-        # hmm...zzz
-        #self.recipe.hops = ['Select', 'Galaxy']
-        brewing_session = brew.Brew(recipe)
-        #brewing_session.loop()
-        print('test finished\n')
-
-if __name__ == '__main__':
-    unittest.main()
+'''
